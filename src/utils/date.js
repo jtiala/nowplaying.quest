8347b4adbc76a3b0d12e82d90dfea682d20a3230
Joonas Tiala <joonas.tiala@gmail.com>
2025-06-30 14:21:23 +0300

    Minor styling

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,4 @@
-export function formatDate(dateStr) {
+export function formatDate(dateStr, nonBreaking = false) {
   if (!dateStr) {
     return "";
   }
@@ -21,6 +21,10 @@ export function formatDate(dateStr) {
   const [year, month, day] = dateStr.split("-");
   const monthName = months[parseInt(month) - 1];
   const dayNum = parseInt(day, 10);
+
+  if (nonBreaking) {
+    return `${monthName}&nbsp;${dayNum},&nbsp;${year}`;
+  }
 
   return `${monthName} ${dayNum}, ${year}`;
 }
