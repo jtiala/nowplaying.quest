5390f18a4688781ecf21cfaf793a730bebc75634
Joonas Tiala <joonas.tiala@gmail.com>
2025-07-01 08:27:53 +0300

    Don't build albums that are set for a future date

--- src/_data/albums.js
+++ src/_data/albums.js
@@ -8,6 +8,8 @@ export default function () {
     return [];
   }
 
+  const includeFuture = process.env.INCLUDE_FUTURE_ALBUMS === "true";
+
   return fs
     .readdirSync(albumDir)
     .filter((f) => f.endsWith(".json"))
@@ -16,6 +18,13 @@ export default function () {
       const album = JSON.parse(fs.readFileSync(path.join(albumDir, f), "utf8"));
 
       return { date, ...album };
+    })
+    .filter((a) => {
+      if (includeFuture) {
+        return true;
+      }
+
+      return new Date(`${a.date}T00:00:00`) <= new Date();
     })
     .sort((a, b) => a.date.localeCompare(b.date));
 }
